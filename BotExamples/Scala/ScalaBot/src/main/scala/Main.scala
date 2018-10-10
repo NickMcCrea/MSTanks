fe46053df65f2aa524542a3ba5e2f4875d2fb91c
Dylan Stevenson <stevensondylan13@googlemail.com>
2018-10-10 01:51:58 +0000

    Add Arg Parseing

--- BotExamples/Scala/ScalaBot/src/main/scala/Main.scala
+++ BotExamples/Scala/ScalaBot/src/main/scala/Main.scala
@@ -3,18 +3,36 @@ import akka.actor.ActorSystem
 import scala.concurrent.duration._
 
 
-object Main extends App {
-
-
-  val system = ActorSystem("TankBot")
-
-  val bot = system.actorOf(Bot.props, "Bot")
-
-  val client = system.actorOf(TcpClient.props( new InetSocketAddress("localhost", 8052), bot), "client")
-
-  import system.dispatcher
-  system.scheduler.schedule(100 milliseconds,1000 milliseconds,bot,"Do")
-
+object Main {
+  
+  def parseArgs(args: Array[String]):Map[String,String] = {
+    args.headOption match {
+      case Some(arg) =>{
+        arg match {
+          case arg if arg.startsWith("-ip") =>
+            Map("ip" -> arg.split("=")(1)) ++ parseArgs(args.tail)
+          case arg if arg.startsWith("-name") =>
+            Map("name" -> arg.split("=")(1)) ++ parseArgs(args.tail)
+          case arg if arg.startsWith("-port") =>
+            Map("port" -> arg.split("=")(1)) ++ parseArgs(args.tail)
+        }
+      }
+      case None => Map.empty
+    }
+  }
+  
+  def main(args: Array[String]): Unit = {
+     val argM:Map[String,String] = parseArgs(args)
+     println(argM)
+     
+     val system = ActorSystem("something")
+     val bot = system.actorOf(Bot.props(argM.getOrElse("name", "scalaBot")), "Bot")
+     val client = system.actorOf(TcpClient.props( new InetSocketAddress(argM.getOrElse("ip","localhost"), argM.getOrElse("port","8052").toInt), bot), "client")
+     
+     import system.dispatcher
+     system.scheduler.schedule(100 milliseconds,1000 milliseconds,bot,"Do")
+   }
+  
 }
 
 
